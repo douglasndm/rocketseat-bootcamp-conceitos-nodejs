18bc78d03d241da94a638107c9ef168d7fd49ee1
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-08-10 03:28:54 -0300

    Finalizando desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,74 @@ app.use(cors());
 
 const repositories = [];
 
+function checkIfRepositoryExists(request, response, next) {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(r => r.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository didn't found" });
+  }
+
+  request.repositoryIndex = repositoryIndex;
+
+  next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", checkIfRepositoryExists, (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const { repositoryIndex } = request;
+
+  const updatedRepository = {
+    ...repositories[repositoryIndex],
+    title,
+    url,
+    techs
+  }
+  repositories[repositoryIndex] = updatedRepository;
+
+  return response.status(200).json(updatedRepository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", checkIfRepositoryExists, (request, response) => {
+  const { repositoryIndex } = request;
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", checkIfRepositoryExists, (request, response) => {
+  const { repositoryIndex } = request;
+
+  const newRepository = {
+    ...repositories[repositoryIndex],
+    likes: repositories[repositoryIndex].likes + 1,
+  };
+
+  repositories[repositoryIndex] = newRepository;
+
+  return response.status(200).json(newRepository);
 });
 
 module.exports = app;
